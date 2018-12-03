2fd4d1030c9a1a75c27e3f60181a48eca7be7d32
liangliang.li <liliang941107@sina.com>
2018-12-03 11:23:43 +0800

    dockerfile

--- FileServer/src/main/java/com/lianglianglee/cloud/file/FileServerApplication.java
+++ FileServer/src/main/java/com/lianglianglee/cloud/file/FileServerApplication.java
@@ -1,4 +1,21 @@
 package com.lianglianglee.cloud.file;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableScheduling
+@ComponentScan(basePackages = {"com.lianglianglee"})
+@MapperScan("com.lianglianglee.cloud.file")
 public class FileServerApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(FileServerApplication.class, args);
+  }
+
 }
